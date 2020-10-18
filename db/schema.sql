a61c4f7a068c8884b094b9956a69858df0a0659a
Heather Everton <heather.everton@planplusonline.com>
2020-10-18 14:05:39 -0600

    update schema & seed files

--- db/schema.sql
+++ db/schema.sql
@@ -2,14 +2,9 @@ DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 USE employee_db;
 
-CREATE TABLE employee (
+CREATE TABLE department (
   id INTEGER(11) auto_increment NOT NULL,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,  
-  role_id INTEGER NOT NULL REFERENCES role(id),
-  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL
-  manager_id INTEGER REFERENCES employee(id),
-  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+  name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
 );
 
@@ -17,13 +12,19 @@ CREATE TABLE role (
   id INTEGER(11) auto_increment NOT NULL,
   title VARCHAR(30) NOT NULL,
   salary decimal NOT NULL,
-  department_id INTEGER NOT NULL REFERENCES department(id),
+  department_id INTEGER REFERENCES department(id),
+  PRIMARY KEY (id),
   CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
-  PRIMARY KEY (id)
+
 );
 
-CREATE TABLE department (
+CREATE TABLE employee (
   id INTEGER(11) auto_increment NOT NULL,
-  name VARCHAR(30) NOT NULL,
-  PRIMARY KEY (id)
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,  
+  role_id INTEGER REFERENCES role(id),
+  manager_id INTEGER REFERENCES employee(id),
+  PRIMARY KEY (id),
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL
+  -- CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,20 +1,13 @@
 USE employee_db;
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES
-  ('John', 'Doe', 1, null),
-  ('Tom', 'Sawyer', 2, 1),
-  ('Huck', 'Finn', 3, 2),
-  ('Jane', 'Austen', 4, null),
-  ('Elizabeth', 'Bennett', 5, 4),
-  ('Anne', 'Shirley', 6, 4);
+
   
 INSERT INTO department (name)
 VALUES
   ('Sales'),
   ('Product');
 
-INSERT INTO roles (title, salary, department_id)
+INSERT INTO role (title, salary, department_id)
 VALUES
   ('VP Sales', 100000, 1),
   ('Sales Director', 70000, 1),
@@ -23,3 +16,12 @@ VALUES
   ('Scrum Master', 90000, 2),
   ('Sr Developer', 120000, 2),
   ('Intern', 30000, 2);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+  ('John', 'Doe', 1, null),
+  ('Tom', 'Sawyer', 2, 1),
+  ('Huck', 'Finn', 3, 2),
+  ('Jane', 'Austen', 4, null),
+  ('Elizabeth', 'Bennett', 5, 4),
+  ('Anne', 'Shirley', 6, 4);
